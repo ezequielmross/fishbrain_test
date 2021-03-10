ccd00ea1bfeaa2a75e5ac237af445e784f825f63
Ezequiel mross <ezequielmross@gmail.com>
2021-03-10 20:40:18 -0300

    Add anglerCatch entity

---
+++ src/entity/anglerCatch.ts
@@ -0,0 +1,31 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from 'typeorm'
+
+@Entity()
+export class AnglerCatch {
+
+    @PrimaryGeneratedColumn()
+    id!: number
+
+    @Column()
+    anglerName!: string
+
+    @Column()
+    species!: string
+
+    @Column()
+    weight!: number
+
+    @Column()
+    length!: number
+
+    @Column()
+    latitude!: number
+
+    @Column()
+    longitude!: number
+
+    @Column("timestamp")
+    timestamp!: Date
+}
+
+export default AnglerCatch
