f6b39ee3de256694435bda5763ee19c31ace1502
Ezequiel mross <ezequielmross@gmail.com>
2021-03-10 23:31:15 -0300

    Fix imageUpload bug

--- src/middlewares/imageUpload.ts
+++ src/middlewares/imageUpload.ts
@@ -18,15 +18,26 @@ const parser = multer({
 export default (fieldName: string) => (req: Request, res: Response, next: Function) => {
   const upload = parser.single(fieldName)
 
+  // Only allows multipart/form-data request
+  const contentType = req.header('content-type')
+  if (!(contentType && contentType.indexOf('multipart/form-data') > -1)) {
+    return res.status(500).json({ error: 'Request must be a multipart/form-data' })
+  }
+
   upload(req, res, (err: any) => {
-      if (err instanceof multer.MulterError) {
-          // A Multer error occurred when uploading.
-          res.status(400).json({ error: err.message })
-      } else if (err) {
-          // An unknown error occurred when uploading.
-          res.status(500).json({ error: err.message })
-      }
-      // Everything went fine.
-      next()
+    // A Multer error occurred when uploading.
+    if (err instanceof multer.MulterError) {
+      return res.status(400).json({ error: err.message })
+    }
+    // An unknown error occurred when uploading.
+    if (err) {
+      return res.status(500).json({ error: err.message })
+    }
+    // Image field is required
+    if (!req.file || !req.file.path) {
+      return res.status(400).json({ error: `Field "${fieldName}" is required` })
+    }
+    // Everything went fine.
+    next()
   })
 }
